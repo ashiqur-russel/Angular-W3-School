6f149c86107157fc106d95a987365742207173ac
ashiqurras <ashiqurrussel2@gmail.com>
2021-11-22 00:23:56 +0100

    Get request

--- AngualJs-http/http.js
+++ AngualJs-http/http.js
@@ -1,6 +1,18 @@
 var app = angular.module("myApp",[]);
+
 app.controller('myCtrl',function($scope,$http){
     $http.get("welcome.htm").then(function(response){
         $scope.myWelcome=response.data;
+    });
+});
+//Get method for http service
+app.controller('myCtrl',function($scope,$http){
+    $http({
+        method:"GET",
+        url :"welcome.htm"
+    }).then(function mySuccess(response){
+        $scope.myWelcome=response.data;
+    },function myError(response){
+        $scope.myWelcome=response.statusText;
     });
 });
