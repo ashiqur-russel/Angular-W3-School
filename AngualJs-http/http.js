222a3ca37082787fe70d02b13e5c153da862ee98
ashiqurras <ashiqurrussel2@gmail.com>
2021-11-22 00:34:59 +0100

    http request for getting json data from server

--- AngualJs-http/http.js
+++ AngualJs-http/http.js
@@ -16,3 +16,20 @@ app.controller('myCtrl',function($scope,$http){
         $scope.myWelcome=response.statusText;
     });
 });
+
+
+
+app.controller('myCtrl1', function($scope, $http) {
+    $http.get("welcome.htm")
+    .then(function(response) {
+        $scope.content = response.data;
+        $scope.statuscode = response.status;
+        $scope.statustext = response.statusText;            
+    });
+});
+//Http request fro getting json data
+app.controller('customersCtrl',function($scope,$http){
+    $http.get("customer.php").then(function(response){
+        $scope.myData=response.data.records;
+    })
+})
