1302c50b80b2f10a7257f530631a1ac8858adec6
ashiqurras <ashiqurrussel2@gmail.com>
2021-11-23 00:32:03 +0100

    Using ng-repeat:

--- AngularJS-Select/select.js
+++ AngularJS-Select/select.js
@@ -3,3 +3,13 @@ var app = angular.module('myApp', []);
 app.controller ('myCtrl',function($scope){
     $scope.names=["Email","Tobias","Linus"];
 })
+
+//ng-repeat directive to create dropdown lists
+
+app.controller('myCar',function($scope){
+$scope.cars=[
+    {model:"Ford Mustang",color:"Red"},
+    {model:"Fiat 500",color:"White"},
+    {model:"Volvo XC90",color:"Black"}
+]
+})
