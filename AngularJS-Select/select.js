f2727881b7fce8743f61cd7b35c91c0b31e72f7b
ashiqurras <ashiqurrussel2@gmail.com>
2021-11-23 00:35:40 +0100

    Using ng-repeat as an object:

--- AngularJS-Select/select.js
+++ AngularJS-Select/select.js
@@ -13,3 +13,13 @@ $scope.cars=[
     {model:"Volvo XC90",color:"Black"}
 ]
 })
+
+//ng-repeat directive as object
+
+app.controller('myCar1',function($scope){
+$scope.cars=[
+    {model:"Ford Mustang",color:"Red"},
+    {model:"Fiat 500",color:"White"},
+    {model:"Volvo XC90",color:"Black"}
+]
+})
