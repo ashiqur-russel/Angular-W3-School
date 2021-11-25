5248c570f39d98c94a54f0e304bba0f9dc2daff3
ashiqurras <ashiqurrussel2@gmail.com>
2021-11-25 11:18:06 +0100

    change state of the add member

--- teamapp/src/app/app.component.ts
+++ teamapp/src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamapp';
-}
+  newMemberName='';
+  members:string[]=[];
+  onInput(member:string){
+    this.newMemberName=member;
+  }
+
+  addMember(){
+    this.members.push(this.newMemberName);
+  }
+ }
