7579ce60ad22918fdb5f6572e666f1a8cddcdee6
ashiqurras <ashiqurrussel2@gmail.com>
2021-11-26 19:02:31 +0100

    method created for generate team

--- teamapp/src/app/app.component.ts
+++ teamapp/src/app/app.component.ts
@@ -6,13 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  //Initaial state of the Application
   newMemberName='';
   members:string[]=[];
+  errorMessage='';
+  numberOfTeams:number | ""= "";
+
+  //function for change input state
   onInput(member:string){
     this.newMemberName=member;
   }
 
+  onNumberOfTeamsInput(value:string){
+    this.numberOfTeams=Number(value);
+  }
+
+  //funtion for add member to array after clicking button
   addMember(){
+    if(!this.newMemberName){
+      this.errorMessage="Name cam't be empty!";
+      return;
+    }
     this.members.push(this.newMemberName);
+    this.newMemberName='';
+  }
+
+  generateTeam(){
+    
+
   }
  }
